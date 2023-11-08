119b6c817dfe3d71aadf8fc3c3bd1b4e6d18a4c9
hujin <hj@mapgis.com>
2023-11-08 15:59:04 +0800

    add log

---
+++ Element.GIS/Logs/ElementLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Element.GIS.Logs
+{
+    public class ElementLogger
+    {
+        public static void Info(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Error(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
